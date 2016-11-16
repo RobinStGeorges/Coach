4c1685d3c0a56ebeb90e51ef1baae4a8d9a544ff
kIRETA <robin.sgeorges@gmail.com>
2016-11-16 15:29:52 +0100

    page 36 N'affiche pas la recherche précedente

--- app/src/main/java/com/example/kireta/coach/vue/MainActivity.java
+++ app/src/main/java/com/example/kireta/coach/vue/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCalc;
     private Controle controle;
 
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
