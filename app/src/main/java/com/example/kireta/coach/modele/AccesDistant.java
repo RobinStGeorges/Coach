fd27fd6c05c9934f51a74fbd65dd64389b678cd3
kIRETA <robin.sgeorges@gmail.com>
2016-12-06 20:21:58 +0100

    page 46 accesDistace, l'affichage console n'affiche pas le bon, acces write file non

--- app/src/main/java/com/example/kireta/coach/modele/AccesDistant.java
+++ app/src/main/java/com/example/kireta/coach/modele/AccesDistant.java
@@ -1,8 +1,44 @@
 package com.example.kireta.coach.modele;
 
+import android.util.Log;
+
+import com.example.kireta.coach.outils.AccesHTTP;
+import com.example.kireta.coach.outils.AsyncResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Created by kIRETA on 06/12/2016.
  */
 
-public class AccesDistant {
+public class AccesDistant implements AsyncResponse {
+    final String SERVERADDR ="http://82.230.54.137/coach/serveurcoach.php";
+
+    public AccesDistant(){
+        super() ;
+    }
+    public void processFinish(String output){
+        Log.d("serveur", "************" + output);
+        String[] message = output.split("%");
+        if (message.length>1){
+            if (message[0]=="enreg"){
+                Log.d("*******************","Enreg"+message);
+            }
+            if (message[0]=="dernier"){
+                Log.d("*******************","Dernier"+message);
+            }
+            if (message[0]=="Erreur !"){
+                Log.d("*******************","Erreur"+message);
+            }
+        }
+    }
+    public void envoi(String operation, JSONArray lesDonneesJSON){
+        AccesHTTP accesDonnees = new AccesHTTP();
+        accesDonnees.delegate = this;
+        accesDonnees.addParam("operation", operation);
+        accesDonnees.addParam("lesdonnees", lesDonneesJSON.toString());
+        accesDonnees.execute(SERVERADDR);
+    }
+
 }
